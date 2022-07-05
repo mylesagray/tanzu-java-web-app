18e64c99effbcd811295082f2511a2b039aab440
Myles Gray <mg@mylesgray.com>
2022-07-05 16:35:34 +0100

    Updated string to TAP 1.2 beta 18

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from TAP 1.2!"));
+            .andExpect(content().string("Greetings from TAP 1.2 beta 18!"));
     }
 }
