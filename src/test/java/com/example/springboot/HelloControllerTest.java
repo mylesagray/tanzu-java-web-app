c8e81e9ff4d852cc2dc3ca8fa31c47bedf601e3a
Myles Gray <mg@mylesgray.com>
2022-07-08 16:24:25 +0100

    Updated string to Tanzu Application Platform

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from TAP 1.2!", controller.index());
+        assertEquals("Greetings from Tanzu Application Platform!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from TAP 1.2!"));
+            .andExpect(content().string("Greetings from Tanzu Application Platform!"));
     }
 }
